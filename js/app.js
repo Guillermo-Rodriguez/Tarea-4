59f2f43b8ec4b13811320812afe90563a5a4a371
Guillermo Rodriguez <alvaradog.antonio600@gmail.com>
2019-08-21 12:37:21 -0600

    Stripe y Paypal en amazon Lite

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+$('#c1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: ("#nc1"),
+    prevArrow: ("#pc1"),
+  });
+  
+  $('#c2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    nextArrow: ("#nc2"),
+    prevArrow: ("#pc2"),
+  });
+  
+  
